63a522927efed34ff6d67a5792bd1db3017d9b2a
Prajwal Saokar <prajwalsaokar@gmail.com>
2023-11-03 04:02:34 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setuptools.setup(
     name="paravastu",  # Replace with your own username
     version="1.3",
     author="Paravastu Lab",
-    long_description="lab package"
+    long_description="lab package",
     packages=setuptools.find_packages(),
     python_requires=">=3.6",
     install_requires=["numpy", "pandas", "biopandas"],
