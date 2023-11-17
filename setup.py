8c10b16e45ff44b93a796866a222b5f1633ad006
Daniel Manogaran <32459657+daniemano@users.noreply.github.com>
2023-11-17 10:11:51 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="paravastu",  # Replace with your own username
-    version="1.3",
+    version="1.4",
     author="Paravastu Lab",
     long_description="lab package",
     packages=setuptools.find_packages(),
